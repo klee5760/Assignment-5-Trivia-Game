5e65e553907cfe6f1596f133ce0e3ff4b15d1e57
Kevin Lee <klee5760@hotmail.com>
2020-01-28 22:09:17 -0700

    first commit

---
+++ triviaquestion.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+var questions = [{
+    question:"Which country and when is the first modern Olympic held?",
+    option1: "Uruguay, 1930",
+    option2: "France, 1900",
+    option3: "United Kingdom, 1916",
+    option4: "Greece, 1896",
+    answer : 4
+},{
+    question:"Who are the two people who won more than three gold medals in winter Olympics in a single game?",
+    option1: "Eric Heiden and Ole Einar Bjorndalen",
+    option2: "Stina Nilsson and Sixten Jernberg",
+    option3: "Marit Bjorgen and Aleksander Bolshunov",
+    option4: "Lyubov Yegorova and Cindy Klassen",
+    answer : 1
+},{
+    question:"Which events in 2016 are the events revived since 1904 and 1924 respectively?",
+    option1: "Cricket and Polo",
+    option2: "Water Polo and Billiards",
+    option3: "Golf and Rugby Sevens",
+    option4: "Tennis and Baseball",
+    answer : 3
+},{
+    question:"Which national capital city and when did it held winter Olympics?",
+    option1: "Tokyo, 1968",
+    option2: "New York, 1924",
+    option3: "Amsterdam, 1948",
+    option4: "Oslo, 1952",
+    answer : 4
+},{
+    question:"Which city was the first city and when did it to be held in Southern Hemisphere?",
+    option1: "Sydney, 2000",
+    option2: "Melbourne, 1956",
+    option3: "Wellington, 1976",
+    option4: "Auckland, 1968",
+    answer : 2
+},{
+    question:"In 1972 Munich Massacre incident, what is the name of terrorist group and which country did they come from?",
+    option1: "Abhinav Bharat, India",
+    option2: "Fatherland and Liberty, Iran",
+    option3: "Black September Organization, Palestine",
+    option4: "Aryan Republican Army, Ireland",
+    answer : 3
+},{
+    question:"Who and when did the Olympic theme 'Bugler's Dream'composed?",
+    option1: "Celine Dion, 1948",
+    option2: "Christopher Cross, 1962",
+    option3: "John Williams, 1968",
+    option4: "Leo Arnaud, 1958",
+    answer : 4
+},{
+    question:"Which city and when did it held the Olympic before and hold again after exactly 100 years?",
+    option1: "Rome, 1936",
+    option2: "Vancouver, 1932",
+    option3: "Los Angeles, 1928",
+    option4: "Paris, 1924",
+    answer : 4
+
+},{
+    question:"The Olympic Hymn was sung originally in Greek. When and what city was the song first sung performed by English?",
+    option1: "Squaw Valley,1960",
+    option2: "Rome, 1960",
+    option3: "Sapporo, 1972",
+    option4: "London, 1948",
+    answer : 1
+},{
+    question:"What was the name and what kind of a character was the first official Olympic mascot?",
+    option1: "Schuss, Stylized Skier",
+    option2: "Schneemann, Snowman",
+    option3: "Waldi,Dachshund Dog",
+    option4: "Vucko, Little Wolf",
+    answer : 3
+
+}]
